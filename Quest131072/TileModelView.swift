7c8b2659f2c1f0625d83ae494c9f3afe7d5b6b5e
Lucas Longo <llongo@linkedin.com>
2025-04-06 15:34:30 -0700

    Removed Watch and TV

--- Quest131072/TileModelView.swift
+++ Quest131072/TileModelView.swift
@@ -25,7 +25,11 @@ struct TileView: View {
             RoundedRectangle(cornerRadius: cellSize * 0.1)
                 .fill(tile.value.colorForValue)
             Text("\(tile.value)")
+            #if os(watchOS)
+                .font(.system(size: cellSize * 0.5, weight: .bold))
+            #else
                 .font(.system(size: cellSize * 0.25, weight: .bold))
+            #endif
                 .foregroundColor(.white)
                 .minimumScaleFactor(0.05) // Adjust the scale factor as needed
                 .lineLimit(1) // Ensure the text is on a single line
